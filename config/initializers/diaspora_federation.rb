15108eec4aba67ae45a0629aa5d86e53a8ece028
Thorsten Claus <thorstenclaus@web.de>
2021-05-05 20:08:56 +0200

    dont finger persons from disabled pods

--- config/initializers/diaspora_federation.rb
+++ config/initializers/diaspora_federation.rb
@@ -15,44 +15,48 @@ DiasporaFederation.configure do |config|
   config.define_callbacks do
     on :fetch_person_for_webfinger do |diaspora_id|
       person = Person.where(diaspora_handle: diaspora_id, closed_account: false).where.not(owner: nil).first
-      unless person.nil? || person.closed_account?
-        DiasporaFederation::Discovery::WebFinger.new(
-          {
-            acct_uri:      "acct:#{person.diaspora_handle}",
-            hcard_url:     AppConfig.url_to(DiasporaFederation::Engine.routes.url_helpers.hcard_path(person.guid)),
-            seed_url:      AppConfig.pod_uri,
-            profile_url:   person.profile_url,
-            atom_url:      person.atom_url,
-            salmon_url:    person.receive_url,
-            subscribe_url: AppConfig.url_to("/people?q={uri}")
-          },
-          aliases: [AppConfig.url_to("/people/#{person.guid}")],
-          links:   [
+      if person
+        unless person.pod&.blocked
+          DiasporaFederation::Discovery::WebFinger.new(
             {
-              rel:  OpenIDConnect::Discovery::Provider::Issuer::REL_VALUE,
-              href: Rails.application.routes.url_helpers.root_url
-            }
-          ]
-        )
+              acct_uri:      "acct:#{person.diaspora_handle}",
+              hcard_url:     AppConfig.url_to(DiasporaFederation::Engine.routes.url_helpers.hcard_path(person.guid)),
+              seed_url:      AppConfig.pod_uri,
+              profile_url:   person.profile_url,
+              atom_url:      person.atom_url,
+              salmon_url:    person.receive_url,
+              subscribe_url: AppConfig.url_to("/people?q={uri}")
+            },
+            aliases: [AppConfig.url_to("/people/#{person.guid}")],
+            links:   [
+              {
+                rel:  OpenIDConnect::Discovery::Provider::Issuer::REL_VALUE,
+                href: Rails.application.routes.url_helpers.root_url
+              }
+            ]
+          )
+        end
       end
     end
 
     on :fetch_person_for_hcard do |guid|
       person = Person.where(guid: guid, closed_account: false).where.not(owner: nil).take
-      unless person.nil? || person.closed_account?
-        DiasporaFederation::Discovery::HCard.new(
-          guid:             person.guid,
-          nickname:         person.username,
-          full_name:        "#{person.profile.first_name} #{person.profile.last_name}".strip,
-          url:              AppConfig.pod_uri,
-          photo_large_url:  person.image_url,
-          photo_medium_url: person.image_url(size: :thumb_medium),
-          photo_small_url:  person.image_url(size: :thumb_small),
-          public_key:       person.serialized_public_key,
-          searchable:       person.searchable,
-          first_name:       person.profile.first_name,
-          last_name:        person.profile.last_name
-        )
+      if person
+        unless person.pod&.blocked
+          DiasporaFederation::Discovery::HCard.new(
+            guid:             person.guid,
+            nickname:         person.username,
+            full_name:        "#{person.profile.first_name} #{person.profile.last_name}".strip,
+            url:              AppConfig.pod_uri,
+            photo_large_url:  person.image_url,
+            photo_medium_url: person.image_url(size: :thumb_medium),
+            photo_small_url:  person.image_url(size: :thumb_small),
+            public_key:       person.serialized_public_key,
+            searchable:       person.searchable,
+            first_name:       person.profile.first_name,
+            last_name:        person.profile.last_name
+          )
+        end
       end
     end
 
