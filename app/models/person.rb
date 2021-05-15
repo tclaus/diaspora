775f7886cf61d7dc64f24b4ac83e5561fb37d58f
Thorsten Claus <thorstenclaus@web.de>
2021-05-15 15:40:19 +0200

    Fix run and test errors

--- app/models/person.rb
+++ app/models/person.rb
@@ -361,7 +361,6 @@ class Person < ApplicationRecord
 
   # discovery (webfinger)
   def self.find_or_fetch_by_identifier(diaspora_id)
-
     return nil if diaspora_handle_from_blocked_pod?(diaspora_id)
 
     # exiting person?
@@ -373,7 +372,6 @@ class Person < ApplicationRecord
     DiasporaFederation::Discovery::Discovery.new(diaspora_id).fetch_and_save
 
     by_account_identifier(diaspora_id)
-
   rescue DiasporaFederation::Discovery::InvalidDocument
     logger.info "#{diaspora_id} returns not as a valid document"
     nil
@@ -451,6 +449,12 @@ class Person < ApplicationRecord
     self
   end
 
+  def self.diaspora_handle_from_blocked_pod?(diaspora_handle)
+    host = diaspora_handle.split("@").last
+    pod = Pod.find_by(host: host)
+    !pod.nil? && pod.blocked
+  end
+
   private
 
   def fix_profile
@@ -467,11 +471,4 @@ class Person < ApplicationRecord
     diaspora_id = Person.where(guid: guid).where.not(diaspora_handle: diaspora_handle).pluck(:diaspora_handle).first
     errors.add(:base, "Person with same GUID already exists: #{diaspora_id}") if diaspora_id
   end
-
-  def self.diaspora_handle_from_blocked_pod?(diaspora_handle)
-    host = diaspora_handle.split('@').last
-    pod = Pod.find_by_host(host)
-    return !pod.nil? && pod.blocked
-  end
-
 end
