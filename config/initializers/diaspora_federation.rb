cb60ac8199f57c77061c75cefc769b6e9edf1e6c
Thorsten Claus <thorstenclaus@web.de>
2021-08-20 23:25:14 +0200

    Close remote accounts in admin UI

--- config/initializers/diaspora_federation.rb
+++ config/initializers/diaspora_federation.rb
@@ -124,6 +124,7 @@ DiasporaFederation.configure do |config|
 
     on :fetch_public_entity do |entity_type, guid|
       entity = Diaspora::Federation::Mappings.model_class_for(entity_type).all_public.find_by(guid: guid)
+      # TODO: Check for person.closed_account? when merging with #8228 'Block Pod'
       case entity
       when Poll
         Diaspora::Federation::Entities.status_message(entity.status_message)
