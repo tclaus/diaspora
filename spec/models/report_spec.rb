72e92da94b6671e65cedcac0035e7aac62c3cebb
Thorsten Claus <thorstenclaus@web.de>
2021-08-26 09:34:38 +0200

    Abstracting reports-action field

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -77,8 +77,8 @@ describe Report, :type => :model do
 
       it "should set an action text on destroy item" do
         @post_report.destroy_reported_item
-        action = @post_report.reload.action
-        expect(action).to eq "Deleted"
+        @post_report.reload
+        expect(@post_report.action_deleted?).to be_truthy
       end
     end
 
@@ -97,8 +97,8 @@ describe Report, :type => :model do
 
       it "should set an action text on destroy item" do
         @post_report.destroy_reported_item
-        action = @post_report.reload.action
-        expect(action).to eq "Deleted"
+        @post_report.reload
+        expect(@post_report.action_deleted?).to be_truthy
       end
     end
   end
