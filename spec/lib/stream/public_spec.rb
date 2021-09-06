5506b9cfe86e4791d087fc2d7958810700d6cc75
Thorsten Claus <thorstenclaus@web.de>
2021-09-06 22:07:35 +0200

    Set a language for posts

--- spec/lib/stream/public_spec.rb
+++ spec/lib/stream/public_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require Rails.root.join('spec', 'shared_behaviors', 'stream')
+require Rails.root.join("spec", "shared_behaviors", "stream")
 
 describe Stream::Public do
   before do
     @stream = Stream::Public.new(alice)
   end
 
-  describe 'shared behaviors' do
-    it_should_behave_like 'it is a stream'
+  describe "shared behaviors" do
+    it_should_behave_like "it is a stream"
   end
 
   describe "#posts" do
