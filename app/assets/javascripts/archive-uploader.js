459c0c71fdd4858f728a0c26c8e024570660c809
Thorsten Claus <thorstenclaus@web.de>
2021-09-08 23:17:07 +0200

    Moving Texts to localization file
    - Updates (most) texts to goobs proposal
    - Moved texts to localization file
    - Fixed file-upload buttons

--- app/assets/javascripts/archive-uploader.js
+++ app/assets/javascripts/archive-uploader.js
@@ -1,24 +1,15 @@
-const checkProfileUploadButton = function() {
-  let photoFiles = $("#profile-file-btn")[0].files;
-  let profileFiles = $("#photos-file-btn")[0].files;
-  if ((photoFiles.size + profileFiles.size) === 0) {
-    $("#upload_profile_files").attr("disabled", "disabled");
-  } else {
-    $("#upload_profile_files").removeAttr("disabled");
-  }
-};
-const profileFileButton = document.getElementById("profile-file-btn");
-const profileFileChosen = document.getElementById("profile-file-chosen");
+const profileFileBtn = document.getElementById('profile-file-btn');
 
-const photosFileButton = document.getElementById("photos-file-btn");
-const photosFileChosen = document.getElementById("photos-file-chosen");
+const profileFileChosen = document.getElementById('profile-file-chosen');
 
-profileFileButton.addEventListener("change", function() {
-  profileFileChosen.textContent = this.files[0].name;
-  checkProfileUploadButton();
-});
+profileFileBtn.addEventListener('change', function(){
+  profileFileChosen.textContent = this.files[0].name
+})
 
-photosFileButton.addEventListener("change", function() {
-  photosFileChosen.textContent = this.files[0].name;
-  checkProfileUploadButton();
-});
+const photosFileBtn = document.getElementById('photos-file-btn');
+
+const photosFileChosen = document.getElementById('photos-file-chosen');
+
+photosFileBtn.addEventListener('change', function(){
+  photosFileChosen.textContent = this.files[0].name
+})
