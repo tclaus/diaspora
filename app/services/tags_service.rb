d72de292edd6f76f5d3b558b793dc44b5cc911c5
Thorsten Claus <thorstenclaus@web.de>
2021-09-12 12:41:25 +0200

    Tag of the day are language dependent

--- app/services/tags_service.rb
+++ app/services/tags_service.rb
@@ -26,12 +26,14 @@ class TagsService
   # Prohibits flooding tags from mass uploading bots
   def popular_tags
     time_span = Time.zone.today - 1.day
+    languages = LanguageService.language_for_public
     ActsAsTaggableOn::Tagging.find_by_sql "select count(*) as count, t.name from
           (select tags.name, posts.author_id from taggings
             left join tags on taggings.tag_id = tags.id
             left join posts on posts.id = taggable_id
             where taggable_type = 'Post' and posts.created_at >= '#{time_span}'
             and posts.public = true
+            and posts.language_id in (#{languages.to_s.delete('[').delete(']').gsub('"', "'")})
             and not exists (Select 1 from ignoring_tags as it where it.name = tags.name)
             group by tags.name, author_id order by tags.name asc) as t group by t.name
             order by count desc
