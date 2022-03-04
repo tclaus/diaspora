3d07decbdc9831164568faa6f68e599b37da73f6
Thorsten Claus <thorstenclaus@web.de>
2022-03-04 17:24:46 +0100

    Hotfixing unknown auhor

--- lib/diaspora/federation/receive.rb
+++ lib/diaspora/federation/receive.rb
@@ -49,7 +49,7 @@ module Diaspora
       def self.comment(entity, opts)
         receive_relayable(Comment, entity, opts) do
           Comment.new(
-            auhor:      author_of(entity),
+            author:      author_of(entity),
             guid:        entity.guid,
             created_at:  entity.created_at,
             text:        entity.text,
