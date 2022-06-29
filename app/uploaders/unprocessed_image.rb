27633fdde97f7f67f19d4a036c22c70ccd61cb78
Thorsten Claus <thorstenclaus@web.de>
2022-06-29 13:19:37 +0200

    Inline single used method

--- app/uploaders/unprocessed_image.rb
+++ app/uploaders/unprocessed_image.rb
@@ -45,14 +45,9 @@ class UnprocessedImage < CarrierWave::Uploader::Base
 
       img = yield(img) if block_given?
 
-      convert_to_storage_format(img) if needs_converting?
+      img.format("webp") if needs_converting?
       img
     end
-  end
-
-  # @param [ImageProcessing::Builder] img
-  def convert_to_storage_format(img)
-    img.format("webp")
   end
 
   version :thumb_small
