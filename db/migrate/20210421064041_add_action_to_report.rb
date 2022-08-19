a28f497ac195e6a6780064d2a3b4021a0bc38ae5
Thorsten Claus <thorstenclaus@web.de>
2022-08-19 23:53:30 +0200

    ignoring rubocop issue

--- db/migrate/20210421064041_add_action_to_report.rb
+++ db/migrate/20210421064041_add_action_to_report.rb
@@ -4,6 +4,8 @@ class AddActionToReport < ActiveRecord::Migration[5.2]
   def change
     add_column :reports, :action, :string
 
+    # rubocop:disable Rails/SkipsModelValidations
     Report.update_all(action: Report::STATUS_NO_ACTION)
+    # rubocop:enable Rails/SkipsModelValidations
   end
 end
