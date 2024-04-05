38880f7636aafa24ca117ac7d9c8d55f5cda2a66
Thorsten Claus <thorstenclaus@web.de>
2024-04-05 15:09:22 +0200

    Filter buttons for Pod view

--- app/assets/javascripts/app/pages/admin_pods.js
+++ app/assets/javascripts/app/pages/admin_pods.js
@@ -4,10 +4,53 @@ app.pages.AdminPods = app.views.Base.extend({
   templateName: "pod_table",
 
   tooltipSelector: "th i",
+  events: {
+    "click #show_all_pods": "showAllPods",
+    "click #show_active_pods": "showActivePods",
+    "click #show_blocked_pods": "showBlockedPods",
+    "click #show_invalid_pods": "showInvalidPods",
+  },
 
   initialize: function() {
     this.pods = new app.collections.Pods(app.parsePreload("pods"));
     this.rows = []; // contains the table row views
+    this.podfilter = 'active';
+  },
+
+  showAllPods: function() {
+    this.podfilter = '';
+    this.postRenderTemplate();
+    this.$("#show_all_pods").addClass("active");
+    this.$("#show_active_pods").removeClass("active");
+    this.$("#show_blocked_pods").removeClass("active");
+    this.$("#show_invalid_pods").removeClass("active");
+  },
+
+  showActivePods: function() {
+    this.podfilter = 'active';
+    this.postRenderTemplate();
+    this.$("#show_all_pods").removeClass("active");
+    this.$("#show_active_pods").addClass("active");
+    this.$("#show_blocked_pods").removeClass("active");
+    this.$("#show_invalid_pods").removeClass("active");
+  },
+
+  showBlockedPods: function() {
+    this.podfilter = 'blocked';
+    this.postRenderTemplate();
+    this.$("#show_all_pods").removeClass("active");
+    this.$("#show_active_pods").removeClass("active");
+    this.$("#show_blocked_pods").addClass("active");
+    this.$("#show_invalid_pods").removeClass("active");
+  },
+
+  showInvalidPods: function() {
+    this.podfilter = 'invalid';
+    this.postRenderTemplate();
+    this.$("#show_all_pods").removeClass("active");
+    this.$("#show_active_pods").removeClass("active");
+    this.$("#show_blocked_pods").removeClass("active");
+    this.$("#show_invalid_pods").addClass("active");
   },
 
   postRenderTemplate: function() {
@@ -16,11 +59,19 @@ app.pages.AdminPods = app.views.Base.extend({
 
     // avoid reflowing the page for every entry
     var fragment = document.createDocumentFragment();
-    this.pods.each(function(pod) {
-      self.rows.push(new app.views.PodEntry({
-        parent: fragment,
-        model: pod
-      }).render());
+    this.$("tbody").empty();
+
+    this.pods.each(function (pod) {
+      if (self.podfilter === '' ||
+        self.podfilter === 'active' && pod.get("status") === "no_errors" ||
+        self.podfilter === 'blocked' && pod.get("blocked") === true ||
+        self.podfilter === 'invalid' && pod.get("status") !== "no_errors") {
+
+        self.rows.push(new app.views.PodEntry({
+          parent: fragment,
+          model: pod
+        }).render());
+      }
     });
     this.$("tbody").append(fragment);
 
@@ -29,6 +80,7 @@ app.pages.AdminPods = app.views.Base.extend({
 
   _showMessages: function() {
     var msgs = document.createDocumentFragment();
+
     if (gon.totalCount && gon.totalCount > 0) {
       let totalPods = $("<div class='alert alert-info' role='alert' />")
         .append(Diaspora.I18n.t("admin.pods.total", {count: gon.totalCount.toLocaleString()}));
@@ -48,20 +100,20 @@ app.pages.AdminPods = app.views.Base.extend({
       msgs.appendChild(totalPods[0]);
     }
 
-    if( gon.uncheckedCount && gon.uncheckedCount > 0 ) {
+    if (gon.uncheckedCount && gon.uncheckedCount > 0) {
       var unchecked = $("<div class='alert alert-info' role='alert' />")
         .append(Diaspora.I18n.t("admin.pods.unchecked", {count: gon.uncheckedCount.toLocaleString()}));
       msgs.appendChild(unchecked[0]);
     }
-    if( gon.versionFailedCount && gon.versionFailedCount > 0 ) {
+    if (gon.versionFailedCount && gon.versionFailedCount > 0) {
       var versionFailed = $("<div class='alert alert-warning' role='alert' />")
-          .append(Diaspora.I18n.t("admin.pods.version_failed", {count: gon.versionFailedCount.toLocaleString()}));
+        .append(Diaspora.I18n.t("admin.pods.version_failed", {count: gon.versionFailedCount.toLocaleString()}));
       msgs.appendChild(versionFailed[0]);
     }
-    if( gon.errorCount && gon.errorCount > 0 ) {
+    if (gon.errorCount && gon.errorCount > 0) {
       var errors = $("<div class='alert alert-danger' role='alert' />")
         .append(Diaspora.I18n.t("admin.pods.errors", {count: gon.errorCount.toLocaleString()}));
-        msgs.appendChild(errors[0]);
+      msgs.appendChild(errors[0]);
     }
 
     if (gon.blockedCount && gon.blockedCount > 0) {
