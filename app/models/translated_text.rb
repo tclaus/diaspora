922dac7fca84b28e9824392fd7bb6657b030f1e2
Thorsten Claus <thorstenclaus@web.de>
2024-04-12 12:05:49 +0200

    Translated texts are now stored in a database

---
+++ app/models/translated_text.rb
@@ -0,0 +1,2 @@
+class TranslatedText < ApplicationRecord
+end
--- app/services/translation_service.rb
+++ app/services/translation_service.rb
@@ -5,24 +5,25 @@ class TranslationService
   require "digest"
 
   def initialize
-    if AppConfig.deepl.enable && AppConfig.deepl.auth_key.present?
-      DeepL.configure do |config|
-        config.auth_key = AppConfig.deepl.auth_key
-      end
+    return unless AppConfig.deepl.enable && AppConfig.deepl.auth_key.present?
+
+    DeepL.configure do |config|
+      config.auth_key = AppConfig.deepl.auth_key
     end
   end
 
   def translate_message(message)
-    translation = translate_text(message.text.to_s)
+    translation = translate_text(message)
     {
-      translatedText:         translation.text,
-      detectedSourceLanguage: translation.detected_source_language
+      translatedText:         translation[:text],
+      detectedSourceLanguage: translation[:detected_source_language]
     }
   end
 
   def enabled?
     return false unless AppConfig.deepl.enable && AppConfig.deepl.auth_key.present?
     return false if Rails.env.test?
+
     enabled_for_locale?
   end
 
@@ -33,21 +34,49 @@ class TranslationService
     supported_languages.any? {|supported_language| supported_language.code.downcase.eql?(local_language) }
   end
 
-  def translate_text(text)
+  def translate_text(message)
     target_language = I18n.locale.to_s.split("_").first
-    hashed_text = Digest::SHA256.hexdigest(text)
-    cache_key = "translatedPost/#{target_language}/#{hashed_text}"
-    Rails.cache.fetch(cache_key) do
-      DeepL.translate text, nil, target_language
+    translated_text = TranslatedText.find_by(message_uid: message.guid, translated_language_id: target_language)
+
+    unless translated_text.nil?
+      increment_hits(translated_text)
+      return {
+        text:                     translated_text.translated_text,
+        detected_source_language: translated_text.original_language_id
+      }
     end
+
+    query_translation_api(message, target_language)
   rescue DeepL::Exceptions::Error
     I18n.t("translation.authkey_not_provided")
   rescue DeepL::Exceptions::RequestError
     I18n.t("translation.translation.error")
   end
 
+  def increment_hits(translated_text)
+    translated_text.increment(:hits)
+    translated_text.save
+  end
+
+  def query_translation_api(message, target_language)
+    translated_text = DeepL.translate message.text, nil, target_language
+    store_translation(translated_text, message, target_language)
+    {
+      text:                     translated_text.text,
+      detected_source_language: translated_text.detected_source_language
+    }
+  end
+
+  def store_translation(translated_text, message, target_language)
+    hashed_text = Digest::SHA256.hexdigest(message.text)
+    TranslatedText.create(message_uid:            message.guid,
+                          hashed_original_text:   hashed_text,
+                          original_language_id:   translated_text.detected_source_language,
+                          translated_text:        translated_text.text,
+                          translated_language_id: target_language)
+  end
+
   def supported_languages
     @supported_languages ||= DeepL.languages
   end
-
 end
