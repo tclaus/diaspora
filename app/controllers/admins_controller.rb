7c4e00cede18a40cf1120bd46c985d766dab61d6
Thorsten Claus <thorstenclaus@web.de>
2024-11-21 12:09:28 +0100

    Show Comments statistics in admin stats page

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -62,7 +62,11 @@ class AdminsController < Admin::AdminController
     @popular_tags = statistics_service.popular_tags_cached
   end
 
-  def user_stats
+  def user_stats_posts
+    @most_active_users = statistics_service.most_active_users
+  end
+
+  def user_stats_comments
     @most_active_users = statistics_service.most_active_users
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -144,7 +144,8 @@ Rails.application.routes.draw do
     get :stats, as: "pod_stats"
     get :post_stats, as: "post_stats"
     get :tag_stats, as: "tag_stats"
-    get :user_stats, as: "user_stats"
+    get :user_stats_posts, as: "user_stats_posts"
+    get :user_stats_comments, as: "user_stats_comments"
     get :dashboard, as: "admin_dashboard"
     get "add_invites/:invite_code_id" => "admins#add_invites", :as => "add_invites"
   end
