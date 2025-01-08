38b70412a4cea05a1739928b69d0f947f70e45ff
Thorsten Claus <thorstenclaus@web.de>
2025-01-08 16:09:54 +0100

    Max 20

--- app/services/spam_statistics_service.rb
+++ app/services/spam_statistics_service.rb
@@ -2,6 +2,6 @@
 
 class SpamStatisticsService
   def self.top_10_highest_spam_score_people
-    Person.where("spam_score >= 0.5").order(spam_score: :desc).limit(10)
+    Person.where("spam_score >= 0.6").order(spam_score: :desc).limit(20)
   end
 end
