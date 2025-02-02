31b855f81aad249d12925faf0f303ed91e7a05b4
Thorsten Claus <thorstenclaus@web.de>
2025-02-02 09:25:27 +0100

    no closed account

--- app/services/spam_statistics_service.rb
+++ app/services/spam_statistics_service.rb
@@ -4,6 +4,7 @@ class SpamStatisticsService
   def self.highest_spam_score_people
     Person.where(spam_score: 0.6..)
           .where.not(owner_id: nil)
+          .where(closed_account: false)
           .order(spam_score: :desc).limit(20)
   end
 end
