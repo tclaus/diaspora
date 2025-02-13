b14a50ab8a37116545c6bbe8c11fc7d0fcb7ef3d
Thorsten Claus <thorstenclaus@web.de>
2025-02-13 20:44:00 +0100

    A comment can only be created once a minute

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -30,6 +30,9 @@ class Comment < ApplicationRecord
 
   validates :text, :presence => true, :length => {:maximum => 65535}
 
+  # don't allow mass creation of comments in a reasonable ammount of time
+  validate :min_time_delay, on: :create
+
   has_many :reports, as: :item
   has_many :comments, foreign_key: :thread_parent_guid, primary_key: :guid
   has_one :signature, class_name: "CommentSignature", dependent: :delete
@@ -95,5 +98,16 @@ class Comment < ApplicationRecord
       self.signature.save
       logger.info "Write a new comment thread parent guid: #{thread_parent_guid}"
     end
+  end
+
+  def min_time_delay
+    return if last_created_comment.created_at < 1.minute.ago
+    errors.add(:base, "Comments can only be created once per minute")
+  end
+
+  def last_created_comment
+    Comment.where(author_id: self.author)
+           .order(created_at: :desc)
+           .first
   end
 end
